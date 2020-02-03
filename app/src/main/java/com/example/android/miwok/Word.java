b25871e7393d75383aef3ffecab268cd88fbc029
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-02-03 21:41:34 +0100

    Continued course.

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,30 @@
+package com.example.android.miwok;
+
+public class Word {
+    private String miwokTranslation;
+    private String defaultTranslation;
+    private int imageResourceId;
+
+    public Word(String miwokTranslation, String defaultTranslation) {
+        this.miwokTranslation = miwokTranslation;
+        this.defaultTranslation = defaultTranslation;
+    }
+
+    public Word(String miwokTranslation, String defaultTranslation, int imageResourceId) {
+        this.miwokTranslation = miwokTranslation;
+        this.defaultTranslation = defaultTranslation;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public String getMiwokTranslation() {
+        return miwokTranslation;
+    }
+
+    public String getDefaultTranslation() {
+        return defaultTranslation;
+    }
+
+    public int getImageResourceIdID() {
+        return imageResourceId;
+    }
+}
---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,54 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class WordAdapter extends ArrayAdapter<Word> {
+    private int backgroundColor;
+
+    public WordAdapter(Activity context, ArrayList<Word> words, int backgroundColor) {
+        super(context, 0, words);
+        this.backgroundColor = backgroundColor;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        Word currentWord = getItem(position);
+
+        TextView miwokTextView = listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        TextView defaultTextView = listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        ImageView imageView = listItemView.findViewById(R.id.word_image_view);
+        if (currentWord.getImageResourceIdID() != 0) {
+            imageView.setImageResource(currentWord.getImageResourceIdID());
+            imageView.setVisibility(View.VISIBLE);
+        }
+        else {
+            imageView.setVisibility(View.GONE);
+        }
+        LinearLayout linearLayout = listItemView.findViewById(R.id.textContainer);
+        int color = ContextCompat.getColor(getContext(), backgroundColor);
+        linearLayout.setBackgroundColor(color);
+
+        return listItemView;
+    }
+}
